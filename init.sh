17804f8a25f57e0c45bde48ddb1a7b307543ab36
y-urotas <yonezawa.satoru.6615@gmail.com>
2019-12-31 23:13:10 +0900

    add init file for install git colorscheme

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# install vim colorscheme
+if [ ! -d ${HOME}/.vim ]; then
+    mkdir ${HOME}/.vim
+fi
+if [ ! -d ${HOME}/.vim/colors ]; then
+    mkdir ${HOME}/.vim/colors
+fi
+if [ ! -d ${HOME}/.vim/colors/papercolor ]; then
+    mkdir ${HOME}/.vim/colors/papercolor
+fi
+
+git clone https://github.com/NLKNguyen/papercolor-theme.git ${HOME}/.vim/colors/papercolor
+if [ $? -ne 0 ]; then
+    echo "ERROR: failed to git clone PaperColor theme"
+    exit 1
+fi
+
+ln -sf ${HOME}/.vim/colors/papercolor/colors/PaperColor.vim ${HOME}/.vim/colors
