c5b3dbd6a6fbd73d6e7351569b4b72c082c5feba
y-urotas <yonezawa.satoru.6615@gmail.com>
2019-12-31 23:12:29 +0900

    use /home/satoru

--- makelist.sh
+++ makelist.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-if [ ! -L ~/.vimrc -a -f ~/.vimrc ]; then
-    mv ~/.vimrc ~/.vimrc.back
+dir="${0%/*}"
+
+if [ ! -L ${HOME}/.vimrc -a -f ${HOME}/.vimrc ]; then
+    mv ${HOME}/.vimrc ${HOME}/.vimrc.back
 fi
 
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ${dir}/.vimrc ${HOME}/.vimrc
 
 exit 0
