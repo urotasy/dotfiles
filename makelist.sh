78af037408b5afca12197d5fc9206449de481c17
y-urotas <yonezawa.satoru.6615@gmail.com>
2020-01-09 23:48:14 +0900

    change file name and use absolute pathname

--- makelist.sh
+++ makelist.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-dir="${0%/*}"
+dir=`cd ${0%/*}; pwd`
 
 if [ ! -L ${HOME}/.vimrc -a -f ${HOME}/.vimrc ]; then
     mv ${HOME}/.vimrc ${HOME}/.vimrc.back
 fi
 
-ln -sf ${dir}/.vimrc ${HOME}/.vimrc
+ln -sf ${dir}/vimrc ${HOME}/.vimrc
 
 exit 0
