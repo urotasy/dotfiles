7d9e4f5734672def2c4f212b4eef85fb18510de2
y-urotas <yonezawa.satoru.6615@gmail.com>
2020-01-11 20:21:41 +0900

    remove source from install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 dir=`cd ${0%/*}; pwd`
 
 # bashrc
 bashrc_end=`tail -n 1 ${HOME}/.bashrc`
-if [ "# dotfiles setting end" != "${bashrc_end}" ]; then
+end_comment="# dotfiles setting end"
+if [ "${bashrc_end}" != "${end_comment}" ]; then
     {
         echo ""
         echo "# Below setting is added by dotfiles"
         echo "source $dir/bashrc"
-        echo "# dotfiles setting end"
+        echo "${end_comment}"
     } >> ${HOME}/.bashrc
 fi
-source ${HOME}/.bashrc
 
 # vimrc
 if [ ! -L ${HOME}/.vimrc -a -f ${HOME}/.vimrc ]; then
