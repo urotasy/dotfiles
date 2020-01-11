eba8c507012d41ed1ebb3862b6e40b41f64e273c
y-urotas <yonezawa.satoru.6615@gmail.com>
2020-01-11 18:52:35 +0900

    add bashrc for prompt setting

---
+++ bashrc
@@ -0,0 +1,2 @@
+# Prompt setting
+export PS1="\[\e[1;36m\][\t] \[\e[32m\]\u@\h\[\e[0m\]: \[\e[34m\]\w\n\[\e[0m\]$ "
--- install.sh
+++ install.sh
@@ -2,10 +2,22 @@
 
 dir=`cd ${0%/*}; pwd`
 
+# bashrc
+bashrc_end=`tail -n 1 ${HOME}/.bashrc`
+if [ "# dotfiles setting end" != "${bashrc_end}" ]; then
+    {
+        echo ""
+        echo "# Below setting is added by dotfiles"
+        echo "source $dir/bashrc"
+        echo "# dotfiles setting end"
+    } >> ${HOME}/.bashrc
+fi
+source ${HOME}/.bashrc
+
+# vimrc
 if [ ! -L ${HOME}/.vimrc -a -f ${HOME}/.vimrc ]; then
     mv ${HOME}/.vimrc ${HOME}/.vimrc.back
 fi
-
 ln -sf ${dir}/vimrc ${HOME}/.vimrc
 
 exit 0
